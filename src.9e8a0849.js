c26ead22e399c962b305998ef222b54fadd7c808
Oleg-Khakhuda <79002829+Oleg-Khakhuda@users.noreply.github.com>
2021-08-23 10:14:49 +0000

    Deploying to gh-pages from @ Oleg-Khakhuda/goit-js-hw-13-image-finder@13677467609558a67c97dfc31eec2682028fa760 🚀

--- src.9e8a0849.js
+++ src.9e8a0849.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-13-image-finder/src.7a2ca104.js.map
+//# sourceMappingURL=/goit-js-hw-13-image-finder/src.9e8a0849.js.map
