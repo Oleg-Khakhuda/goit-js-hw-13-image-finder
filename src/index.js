a5d5dd235a0c76723d4251594280cebd34538773
Oleg-Khakhuda <buddy2317@gmail.com>
2021-08-24 11:32:59 +0300

    Добавляет разметку Fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
+import NewsApiService from './js/apiService';
+import LoadMoreBtn from './js/components/load-more-btn';
+import articlesTpl from './templates/articles.hbs';
 import './sass/main.scss';
+
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  articlesContainer: document.querySelector('.gallery'),
+};
+
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+const newsApiService = new NewsApiService();
+
+refs.searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.refs.button.addEventListener('click', fetchArticles);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  newsApiService.query = e.currentTarget.elements.query.value;
+
+  if (newsApiService.query === '') {
+    return alert('Введи что-то нормальное');
+  }
+
+  loadMoreBtn.show();
+  newsApiService.resetPage();
+  clearArticlesContainer();
+  fetchArticles();
+}
+
+function fetchArticles() {
+  loadMoreBtn.disable();
+  newsApiService.fetchArticles().then(articles => {
+    appendArticlesMarkup(articles);
+    loadMoreBtn.enable();
+  });
+}
+
+function appendArticlesMarkup(articles) {
+  refs.articlesContainer.insertAdjacentHTML('beforeend', articlesTpl(articles));
+}
+
+function clearArticlesContainer() {
+  refs.articlesContainer.innerHTML = '';
+}
