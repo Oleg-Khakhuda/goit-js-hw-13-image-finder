55692a2033dcdf29a9b56123bf8668d4cbcc4995
Oleg-Khakhuda <buddy2317@gmail.com>
2021-08-24 12:43:46 +0300

    Добавляет basicLightbox

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import NewsApiService from './js/apiService';
 import LoadMoreBtn from './js/components/load-more-btn';
 import articlesTpl from './templates/articles.hbs';
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
+
 import './sass/main.scss';
 
 const refs = {
   searchForm: document.querySelector('.search-form'),
-  articlesContainer: document.querySelector('.gallery'),
+    articlesContainer: document.querySelector('.gallery'),
+    cardsList: document.querySelector('.gallery')
 };
+
+
 
 const loadMoreBtn = new LoadMoreBtn({
   selector: '[data-action="load-more"]',
@@ -14,8 +20,10 @@ const loadMoreBtn = new LoadMoreBtn({
 });
 const newsApiService = new NewsApiService();
 
+
 refs.searchForm.addEventListener('submit', onSearch);
 loadMoreBtn.refs.button.addEventListener('click', fetchArticles);
+refs.cardsList.addEventListener('click', openModal);
 
 function onSearch(e) {
   e.preventDefault();
@@ -46,4 +54,14 @@ function appendArticlesMarkup(articles) {
 
 function clearArticlesContainer() {
   refs.articlesContainer.innerHTML = '';
+}
+
+function openModal(event) {
+    if (!event.target.classList.contains('image')) {
+        return
+    }
+    const instance = basicLightbox.create(
+            `<img src="${event.target.dataset.src}">`,
+    );
+        instance.show();
 }
